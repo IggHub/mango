315660cbbfe6234ea26f5527a1517f43da72c2ae
Iggy <igoririanto@rocketmail.com>
2018-08-18 12:35:04 -0500

    create product_card template, product view, added csv reading capability, and added images

--- test/mango/acceptance/category_page_test.exs
+++ test/mango/acceptance/category_page_test.exs
@@ -1,10 +1,14 @@
 defmodule MangoWeb.Acceptance.CategoryPageTest do
-  use ExUnit.Case
+  use Mango.DataCase
   use Hound.Helpers
 
   hound_session()
 
   setup do
+    alias Mango.Repo
+    alias Mango.Catalog.Product
+    Repo.insert %Product{name: "Tomato", price: 50, is_seasonal: false, category: "vegetables"}
+    Repo.insert %Product{name: "Apple", price: 100, is_seasonal: true, category: "fruits"}
     :ok
   end
 
--- test/mango/acceptance/homepage_test.exs
+++ test/mango/acceptance/homepage_test.exs
@@ -1,15 +1,20 @@
-defmodule MangoWeb.HomepageTest do
-  use ExUnit.Case
+defmodule MangoWeb.Acceptance.HomepageTest do
+  use Mango.DataCase
   use Hound.Helpers
 
   hound_session()
 
+  setup do
+    alias Mango.Repo
+    alias Mango.Catalog.Product
+    Repo.insert %Product{name: "Tomato", price: 50, is_seasonal: false}
+    Repo.insert %Product{name: "Apple", price: 100, is_seasonal: true}
+    :ok
+  end
+
   test "presence of featured products" do
-    # WHEN
-    # I navigate to homepage
     navigate_to("/")
 
-    # THEN
     page_title = find_element(:css, ".page-title") |> visible_text
     assert page_title == "Seasonal Products" 
     product = find_element(:css, ".product")
@@ -17,7 +22,7 @@ defmodule MangoWeb.HomepageTest do
     product_price = find_within_element(product, :css, ".product-price") |> visible_text
 
     assert product_name == "Apple"
-    assert product_price == "100"
+    assert product_price =~ "100"
 
     refute page_source() =~ "Tomato"
   end
