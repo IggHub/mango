e419f0a902f4010a4ed9cbef31e862f543c41ccd
Iggy <igoririanto@rocketmail.com>
2018-08-21 18:10:50 -0500

    create session router, controller, and template

--- lib/mango_web/router.ex
+++ lib/mango_web/router.ex
@@ -20,6 +20,9 @@ defmodule MangoWeb.Router do
     get "/categories/:name", CategoryController, :show
     get "/register", RegistrationController, :new
     post "/register", RegistrationController, :create
+
+    get "/login", SessionController, :new
+    post "/login", SessionController, :create
   end
 
   # Other scopes may use custom stacks.
